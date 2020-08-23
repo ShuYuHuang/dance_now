29a25f7ab51e01d10b8b36c50a1a03e18e4261ca
AT091094黃書璵 <b123767195@gmail.com>
2020-08-23 11:12:46 +0800

    delete data folder

---
+++ initiate.sh
@@ -0,0 +1,10 @@
+#pip install pathlib
+#pip install pytube3
+#pip install pycocotools
+pip install torchvision==0.3.0
+pip install torch==1.1.0
+pip install yacs
+pip install dominate
+pip install scikit-image
+#Fix error
+#%cp extract.py /opt/conda/lib/python3.7/site-packages/pytube/
